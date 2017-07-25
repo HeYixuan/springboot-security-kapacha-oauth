26d7094525d243f912b3d51e403e0b10635bdf23
HeYixuan <heyixuan@igetwell.cn>
2017-07-25 11:09:04 +0800

    添加redis

--- micmiu-idea-security/src/main/java/org/springframe/listener/AuthenticationFailureListener.java
+++ micmiu-idea-security/src/main/java/org/springframe/listener/AuthenticationFailureListener.java
@@ -3,8 +3,11 @@ package org.springframe.listener;
 import org.springframe.system.service.SystemUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 登陆失败监听
@@ -17,11 +20,21 @@ public class AuthenticationFailureListener implements ApplicationListener<Authen
 
     @Autowired
     private SystemUserService systemUserService;
+    @Autowired
+    private static RedisTemplate<String, String> redisTemplate;
+    private int attempts = 0;
     @Override
     public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent event) {
         String username = event.getAuthentication().getName();
+        attempts++;
+        redisTemplate.opsForValue().set(username, String.valueOf(attempts), 10L, TimeUnit.MINUTES);
+        int lockAttempts = Integer.parseInt(redisTemplate.opsForValue().get(username));
+        if (lockAttempts>=3){
+            System.err.println("用户"+ username + "锁定");
+            systemUserService.updateAttempts(username);
+        }
         System.err.println("用户"+ username + "登录失败次数+1");
-        systemUserService.updateAttempts(username);
+
 
     }
 }
